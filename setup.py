307c6d5ecbc1a9489a08918d0b71823305d69e66
Solgema <martronic@martronic.ch>
2012-06-28 15:35:25 +0200

    Autoinclude

--- setup.py
+++ setup.py
@@ -29,5 +29,7 @@ setup(name='Solgema.ContextualContentMenu',
       ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
